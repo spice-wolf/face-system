a0c7aa04fc566ac959b9e5213c7c84650413f167
3119005024 <17806757663@163.com>
2022-11-03 17:11:43 +0800

    feat(file-service): 初始化文件服务

---
+++ file-service/src/main/java/com/spice/fileservice/FileServiceApplication.java
@@ -0,0 +1,15 @@
+package com.spice.fileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author spice
+ */
+@SpringBootApplication
+public class FileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileServiceApplication.class, args);
+    }
+}
